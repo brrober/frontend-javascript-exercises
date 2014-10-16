99fecd57ce352572642d13951fb93b4d837a7ab6
brrober <brrober@gmail.com>
2014-10-16 15:33:53 +0000

    object work completed

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,30 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+  var course = {
+    title: courseTitle,
+    duration: courseDuration,
+    students: courseStudents
+  }
+  return course;
+}
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+  if (!object[newProp]) {
+    object[newProp] = newValue;
+  }
+     return object;
+  }
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter) {
+  var formattedLetter = "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+  return formattedLetter;
+}
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+  var obj={
+    'MacBook Air' : 999,
+    'MacBook Pro' : 1299,
+    'Mac Pro' :2499,
+    'Apple Sticker' : 1
+  }
+  return obj[item]<=money;
+  }
